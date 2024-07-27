0741075bd9901cb2f5e550a4b71a848d08161a2f
SRT-Online <101435248+SRT-Online@users.noreply.github.com>
2024-07-27 11:51:52 +0100

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -38,13 +38,26 @@ xui.Class('App', 'xui.Module',{
             append(
                 xui.create("xui.UI.Input")
                 .setHost(host,"xui_ui_input6")
-                .setLeft("21.333333333333332em")
+                .setLeft("17.523809523809526em")
                 .setTop("9.142857142857142em")
-                .setWidth("18em")
-                .setHeight("10em")
+                .setWidth("23.314285714285713em")
+                .setHeight("2.361904761904762em")
                 .setLabelSize("8em")
-                .setLabelCaption("Text Area")
+                .setLabelCaption("Username")
                 .setMultiLines(true)
+            );
+            
+            append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input8")
+                .setLeft("19.047619047619047em")
+                .setTop("12.19047619047619em")
+                .setWidth("21.79047619047619em")
+                .setHeight("2.5142857142857142em")
+                .setLabelSize("8em")
+                .setLabelCaption("Password Input")
+                .setType("password")
+                .setValue("1234")
             );
             
             return children;
